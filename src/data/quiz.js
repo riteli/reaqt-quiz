dda95b7df8e66f83aa6107018dbf3c758470d53e
Ritsu <r.ogata.0629@gmail.com>
2025-05-14 10:21:07 +0900

    first commit

---
+++ src/data/quiz.js
@@ -0,0 +1,29 @@
+const quizData = [
+  {
+    question: "「世界で一番高い山」は何？",
+    options: ["エベレスト", "キリマンジャロ", "マッターホルン", "モンブラン"],
+    answerIndex: 0
+  },
+  {
+    question: "日本で一番長い川はどれ？",
+    options: ["利根川", "信濃川", "木曽川", "淀川"],
+    answerIndex: 1
+  },
+  {
+    question: "ローマ数字で「50」を表す文字は？",
+    options: ["L", "X", "C", "V"],
+    answerIndex: 0
+  },
+  {
+    question: "「赤ずきんちゃん」を書いたのは誰？",
+    options: ["グリム兄弟", "アンデルセン", "シェイクスピア", "シャルル・ペロー"],
+    answerIndex: 3
+  },
+  {
+    question: "「酸素」の化学記号はどれ？",
+    options: ["S", "CO", "O", "O2"],
+    answerIndex: 2
+  },
+];
+
+export default quizData;
